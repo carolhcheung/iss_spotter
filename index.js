c03c02f191a4cb2af3010856b51c2e5348390791
Carol Cheung <carol.cheung@telus.com>
2022-11-24 15:27:12 -0800

    add flyover times

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // index.js
-const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -17,4 +17,15 @@ fetchCoordsByIP('162.245.144.188', (error, coordinates) => {
   }
 
   console.log('It worked! Returned coordinates:' , coordinates);
+});
+
+const exampleCoords = { latitude: '49.27670', longitude: '-123.13000' };
+
+fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned flyover times:' , passTimes);
 });
