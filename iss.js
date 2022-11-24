43210b066017b8067b4db747248faaa9d15f11f0
Carol Cheung <carol.cheung@telus.com>
2022-11-24 14:22:32 -0800

    iss.js updated

--- iss.js
+++ iss.js
@@ -15,7 +15,15 @@ const fetchMyIP = (callback) => {
     if (error) {
       return callback(error, null);
     }
+
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
     callback(null, IP);
+
   });
 };
 
